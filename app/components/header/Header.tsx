722da35f98bebef2182b4a53559e5fd7ab20da68
Neh Joshi <nehjoshi5@gmail.com>
2025-07-24 15:21:37 -0400

    Initial commit

---
+++ app/components/header/Header.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import styles from "./Header.module.scss";
+
+export default function Header() {
+  return (
+    <nav className={styles.container}>
+      <span className={styles.initials}>NJ</span>
+      <ul className={styles.navLinks}>
+        <li className={styles.navLink}>
+          <Link href="/">Resume</Link>
+        </li>
+        <li className={styles.navLink}>
+          <Link href="/">Blog</Link>
+        </li>
+        <li className={styles.navLink}>
+          <Link href="/">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
