3a5cb561d791f29ffc14127ba26ef2d97329afae
Neh Joshi <nehjoshi5@gmail.com>
2025-08-23 17:49:49 +0100

    Fixed footer links

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,5 +1,13 @@
 import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 import styles from "./Footer.module.scss";
+import Link from 'next/link';
+import React from "react";
+
+const linkProps = {
+    target: "_blank",
+    referrerPolicy: "no-referrer" as React.HTMLAttributeReferrerPolicy,
+    rel: "noopener noreferrer"
+};
 
 export default function Footer() {
     return (
@@ -9,9 +17,9 @@ export default function Footer() {
                 <span>Neh Joshi | Portfolio</span>
             </div>
             <div className={styles.socialIconContainer}>
-                <FaInstagram className={styles.socialIcon}/>
-                <FaLinkedin className={styles.socialIcon}/>
-                <FaGithub className={styles.socialIcon} />
+                <Link href={"https://instagram.com/neh.joshi.98"} {...linkProps}><FaInstagram className={styles.socialIcon}/></Link>
+                <Link href={"https://linkedin.com/in/nehjoshi"} {...linkProps}><FaLinkedin className={styles.socialIcon}/></Link>
+                <Link href={"https://github.com/nehjoshi"} {...linkProps}><FaGithub className={styles.socialIcon} /></Link>
             </div>
         </footer>
     )
